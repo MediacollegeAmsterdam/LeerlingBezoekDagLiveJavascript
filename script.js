bbe8525a9897e926b22ad23738007142c00af49e
Hidde Braun <hidde.braun@gmail.com>
2019-10-03 15:37:59 +0200

    Start script

--- script.js
+++ script.js
@@ -6,16 +6,8 @@ canvas.height = window.innerHeight;
 
 function animate(){
   requestAnimationFrame(animate)
-  context.beginPath();
-  context.arc(getRandom(canvas.width),getRandom(canvas.height),10,0,Math.PI*2);
-  context.fillStyle = "red";
-  context.stroke();
-  context.fill();
-  context.closePath();
+  console.log("animate!");
 }
 
-animate()
+animate();
 
-function getRandom(max){
-  return Math.floor(Math.random()*max)
-}
